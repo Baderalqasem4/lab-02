2801277ee9435ada8cd0aed7aa019c043cc636a9
Bader <baderalq774@gmail.com>
2025-09-11 13:29:03 -0600

    Add Android Studio project files for lab 2

---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declare the variables so that you will be able to reference it later.
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+    Button addButton;
+    Button deleteButton;
+    EditText inputCity;
+    Button confirmButton;
+    LinearLayout bottomBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        cityList = findViewById(R.id.city_list);
+        addButton = findViewById(R.id.add_button);
+        deleteButton = findViewById(R.id.delete_button);
+        inputCity = findViewById(R.id.input_city);
+        confirmButton = this.findViewById(R.id.confirm_button);
+        bottomBar = findViewById(R.id.bottom_bar);
+
+        dataList = new ArrayList<>();
+
+        cityAdapter = new ArrayAdapter<>( this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        cityList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        addButton.setOnClickListener(v -> {
+            bottomBar.setVisibility(View.VISIBLE); // make bottom visible (text box and confirm button)
+            inputCity.requestFocus(); // move cursor into the text box
+        });
+
+        confirmButton.setOnClickListener(v -> {
+            String cityAdded = inputCity.getText().toString();
+            dataList.add(cityAdded);
+            cityAdapter.notifyDataSetChanged(); // refresh
+            inputCity.getText().clear();
+            bottomBar.setVisibility(View.GONE);
+        });
+
+        deleteButton.setOnClickListener(v -> {
+            int position = cityList.getCheckedItemPosition();  // get index of selected row
+            dataList.remove(position);                     // remove from your ArrayList
+            cityAdapter.notifyDataSetChanged();            // refresh the UI
+            cityList.clearChoices();                       // clear the highlight
+        });
+    }
+}
